95af9fea24532fc38181485f16c66126898585ce
nohyoungjin <45346737+nohyoungjin@users.noreply.github.com>
2023-03-10 14:02:14 +0900

    프리즈마 설치

--- react-project/src/pages/api/data.js
+++ react-project/src/pages/api/data.js
@@ -4,8 +4,8 @@ const data = [
     { id: 1, name: 'John', age: 30 },
     { id: 2, name: 'Jane', age: 25 },
     { id: 3, name: 'Bob', age: 40 },
-  ]
+]
   
-  export default (req, res) => {
-    res.status(200).json(data);
-  }
+export default (req, res) => {
+  res.status(200).json(data);
+}
